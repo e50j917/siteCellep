8285c72d835851663ce718533d90d6aebdc4b32e
e50j917 <86218864+e50j917@users.noreply.github.com>
2022-06-18 19:03:11 -0300

    Add files via upload
    Adicionando arquivos adicionais

---
+++ script.js
@@ -0,0 +1,24 @@
+// Script por Israel Santos
+
+var myButtonModal = document.getElementById("myButtonModal")
+var myTextModal = document.getElementById("myTextModal")
+
+var inputUsuario = document.getElementById("inputUsuario")
+var inputSenha = document.getElementById("inputSenha")
+
+function autenticar(event){  
+  // interromper o submit
+  event.preventDefault()
+  // Consistencias
+  if(inputUsuario.value == "root" && inputSenha.value == "1234"){
+    myTextModal.className = "text-success"
+    myTextModal.innerText = "Usuário Logado!!"
+  } else{
+    myTextModal.className = "text-danger"
+    myTextModal.innerText = "Usuário ou senha inválido!!"
+  }
+
+  myButtonModal.click()
+}
+
+
